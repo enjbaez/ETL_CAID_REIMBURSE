22c3d06ef30145070c794e34e7e262f0f9c6766f
enjbaez <enjoli.baez@gmail.com>
2020-04-08 11:14:10 -0400

    web style

---
+++ schema.sql
@@ -0,0 +1,19 @@
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/nEsszZ
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "tbl11_18" (
+    "state" VARCHAR,
+    "FY_2012_Total_BENEFICIARIES" INT,
+    "NONE_SPECIFIED_x" INT,
+    "EARLY_AND_PERIODIC_SCREENING_DIAGNOSIS_AND_TREATMENT_SERVICES_x" INT,
+    "FAMILY_PLANNING_SERVICES_x" INT,
+    "RURAL_HEALTH_CLINICS_SERVICES_x" INT,
+    "FEDERALLY_QUALIFIED_HEALTH_CENTERS_x" INT,
+    "INDIAN_HEALTH_SERVICES_x" INT,
+    "HOME_COMMUNITY_BASED_WAIVER_SERVICES_FOR_DISABLED_ELDERLY_x" INT,
+    "HOME_COMMUNITY_BASED_WAIVER_SERVICES_x" INT,
+    "UNKNOWN" VARCHAR
+);
+
